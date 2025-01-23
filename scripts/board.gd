0bc9201481e91564f013ef74eb8f1fa19ee246f8
Tiago Oliveira <tiagofelipe@alunos.utfpr.edu.br>
2025-01-23 09:33:43 -0300

    Adicionado as mecanincas de restrição e removido bordas pretas que geravam por conta da resolução: testado em 16:9 e 20:9

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,93 +1,143 @@
 extends Area2D
 
 #OBS: Precisa generalizar para funcionar em tabuleiros 3x3, 4x4, 5x5
+var grid_size: int = 4
+var tile_count: int = grid_size * grid_size
+const BOARD_OFFSET_X: int = 3 #Espaço da borda da tela
+const BOARD_OFFSET_Y: int = 42
 
-const GRID_SIZE = 4
-const TILE_COUNT = GRID_SIZE * GRID_SIZE
-const TILE_SIZE = 20 #Tamanho em pixel
-const BOARD_OFFSET_X = 5 #Espaço da borda da tela
-const BOARD_OFFSET_Y = 40
-
-var tiles = []
-var solved = []
-var expected_rows = []
+var scale_tile: float = 0.035
+var tile_size: float = 600 * scale_tile
+var tiles: Array = []
+var solved: Array = []
+var expected_rows: Array = []
+var restrictions: Array = [] 
+var fences: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var screen_size: Vector2 = get_viewport().get_visible_rect().size
+	var tile: Sprite2D = $Frutas/Tile0
+	var sprite: Sprite2D = $Background
+
+	var texture_size: Vector2 = sprite.texture.get_size()
+
+	# Calcular escala
+	var scale_x: float = screen_size.x / texture_size.x
+	var scale_y: float = screen_size.y / texture_size.y
+
+	var scale_factor: float = max(scale_x, scale_y)
+
+	# Ajustar o sprite com o fator de escala
+	sprite.scale = Vector2(scale_factor, scale_factor)
+	
+	# Ajustar a posição do sprite para manter o fundo centralizado
+	var offset_x: float = (screen_size.x - texture_size.x * scale_factor) / 2
+	var offset_y: float = (screen_size.y - texture_size.y * scale_factor) / 2
+	sprite.position = Vector2(offset_x + 42, offset_y + 58)
+	
 	start_game()
 
-func start_game():
+func start_game() -> void:
 	#Referencia para os sprites
 	tiles = [
-		$Tile1, $Tile2, $Tile3, $Tile4,
-		$Tile5, $Tile6, $Tile7, $Tile8,
-		$Tile9, $Tile10, $Tile11, $Tile12,
-		$Tile13, $Tile14, $Tile15, $Tile16
+		$Frutas/Tile1, $Frutas/Tile2, $Frutas/Tile3, $Frutas/Tile4,
+		$Frutas/Tile5, $Frutas/Tile6, $Frutas/Tile7, $Frutas/Tile8,
+		$Frutas/Tile9, $Frutas/Tile10, $Frutas/Tile11, $Frutas/Tile12,
+		$Frutas/Tile13, $Frutas/Tile14, $Frutas/Tile15, $Frutas/Tile0
 	]
-	solved = tiles.duplicate() #Cópia ordenada para verificar se o nivel foi completado
+	restrictions = [
+		"free", "free", "locked", "horizontal_locked",
+		"free", "free", "free", "free",
+		"free", "vertical_locked", "free", "free",
+		"free", "free", "free", "free",
+	]
 
-
-	var aux = []
-	for i in range(0, 16):
+	var aux: Array = []
+	for i in range(0, tile_count):
 		aux.append(tiles[i].name)
-		if (i + 1) % 4 == 0:
+		if (i + 1) % grid_size == 0:
 			expected_rows.append(aux)
 			aux = []
 
+	instantiate_fences(restrictions)
 	shuffle_tiles()
 
+func instantiate_fences(restrictions: Array) -> void:
+	#Dependendo do tipo de restrição instancia a cena da cerca
+	var scene: PackedScene
+	for i in range(len(restrictions)):
+		if restrictions[i] == "free":
+			fences.append(null)
+			continue
+		elif restrictions[i] == "locked":
+			scene = load("res://actors/fullFence.tscn") 
+			
+		elif restrictions[i] == "horizontal_locked":
+			scene = load("res://actors/verticalFence.tscn")
+
+		elif restrictions[i] == "vertical_locked":
+			scene = load("res://actors/horizontalFence.tscn")
+
+		var instance: Node2D = scene.instantiate()			
+		#Calcula a posição da cerca na tela de acordo com a posição da restrição
+		instance.position.x += (i % grid_size) * tile_size
+		instance.position.y += (i / grid_size) * tile_size
+		add_child(instance)
+		fences.append(instance)
+
 #Procura pelo espaço vazio
-func find_empty():
-	var count = 0
+func find_empty() -> int:
+	var count: int = 0
 	for t in tiles:
-		if t == $Tile16:
+		if t == $Frutas/Tile0:
 			break
 		count += 1
 
 	return count
 
 #Verifica quais são os vizinhos ortoganais do espaço vazinho
-func empty_neighbours(empty):
-	var directions = [
-		-GRID_SIZE,   # baixo
-		GRID_SIZE,  # cima
+func empty_neighbours(empty: int, restrictions: Array) -> Array:
+	var directions: Array = [
+		-grid_size,   # baixo
+		grid_size,  # cima
 		1,   # direita
 		-1   # esquerda
 	]
 
-	var neighbours = []
+	var neighbours: Array = []
 	for direction in directions:
-		var neighbour = empty + direction
+		var neighbour: int = empty + direction
 
-		if neighbour < 0 or neighbour >= 16:
+		#Ignora vizinhos que estrapolam os limites do tabuleiro
+		if neighbour < 0 or neighbour >= tile_count:
 			continue
-
-		#Não é necessario? Impediria que as peças da primeira coluna fossem
-		#Movida para a esquerda e da ultima para a direita
-		"""
-		if direction == 1 and empty % GRID_SIZE == GRID_SIZE - 1:
+		
+		#Caso haja uma restrição horizontal, ignora as movimentação para esquerda e direita
+		if direction in [1, -1] and restrictions[neighbour] == "horizontal_locked":
 			continue
-		if direction == -1 and empty % GRID_SIZE == 0:
+			
+		#Caso haja uma restrição vertical, ignora as movimentação para cima e baixo
+		if direction in [grid_size, -grid_size] and restrictions[neighbour] == "vertical_locked":
 			continue
-		"""
-
+			
 		neighbours.push_back(neighbour)
 
 	return neighbours
 
 #Escolhe um dos vizinho do espaço vazio a ser movido (Provavelmente sera retirado
 #E usado nivel criado manualmente
-func choose_neighbour(neighbours):
+func choose_neighbour(neighbours: Array) -> int:
 	randomize()
-	var random_index = randi() % neighbours.size()
+	var random_index: int = randi() % neighbours.size()
 	return neighbours[random_index]
 
-func shuffle_tiles():
+func shuffle_tiles() -> void:
 	#Bem direto, acha o espaço vazio, ve quem são os vizinho e escolhe aleatoriamente um deles
 	for t in range(1000):
-		var empty = find_empty()
-		var neighbours = empty_neighbours(empty)
-		var moved_tile = choose_neighbour(neighbours)
+		var empty: int = find_empty()
+		var neighbours: Array = empty_neighbours(empty, restrictions)
+		var moved_tile: int = choose_neighbour(neighbours)
 
 		swap_tiles(empty, moved_tile)
 
@@ -97,29 +147,60 @@ func _process(delta: float) -> void:
 
 func is_valid_position(position: Vector2) -> bool:
 	#Restringe o clique apenas a região do tabuleiro
-	return position.x >= 3 and position.x < 83 and position.y >= 42 and position.y < 122
+	#84x84 é o tamanho do tabuleiro
+	return position.x >= BOARD_OFFSET_X and position.x < BOARD_OFFSET_X + 84  \
+	and position.y >= BOARD_OFFSET_Y and position.y < BOARD_OFFSET_Y + 84
 
-func swap_tiles(tile_src, tile_dst):
-	#Troca de posição da peça pelo espaço vazio no sprite
-	var temp_pos = tiles[tile_src].position
+func swap_tiles(tile_src: int, tile_dst: int) -> void:
+	#Troca a posição da peça pelo espaço vazio no sprite
+	var temp_pos: Vector2 = tiles[tile_src].position
 	tiles[tile_src].position = tiles[tile_dst].position
 	tiles[tile_dst].position = temp_pos
 
-	#Troca a posição da peça pelo espaço vazio no vetor
-	var temp_tile = tiles[tile_src]
+	#Troca de posição da peça pelo espaço vazio no vetor
+	var temp_tile: Sprite2D = tiles[tile_src]
 	tiles[tile_src] = tiles[tile_dst]
 	tiles[tile_dst] = temp_tile
+	
+	#Troca as posições das restrições
+	var temp_restriction: String = restrictions[tile_src]
+	restrictions[tile_src] = restrictions[tile_dst]
+	restrictions[tile_dst] = temp_restriction
+	
+	#print(fences)
+	#print(temp_restriction)
+	#		instance.position.x += (i % grid_size) * tile_size
+	#	instance.position.y += (i / grid_size) * tile_size
+	print(tile_src)
+	print(tile_dst)
+	print("\n")
+	var temp_fence: Node2D = fences[tile_src]
+	fences[tile_src] = fences[tile_dst]
+	fences[tile_dst] = temp_fence
 
-func handle_mouse_click(mouse_position):
+	# Atualize a posição dos sprites de fences
+	if fences[tile_src] != null and fences[tile_src].name != "free":
+		fences[tile_src].position.x = (tile_src % grid_size) * tile_size
+		fences[tile_src].position.y = (tile_src / grid_size) * tile_size
+
+	if fences[tile_dst] != null and fences[tile_dst].name != "free":
+		fences[tile_dst].position.x = (tile_dst % grid_size) * tile_size
+		fences[tile_dst].position.y = (tile_dst / grid_size) * tile_size
+
+func handle_mouse_click(mouse_position: Vector2) -> void:
 	if !is_valid_position(mouse_position):
 		return
 
-	var rows = int((mouse_position.y - BOARD_OFFSET_Y) / (TILE_SIZE))
-	var cols = int((mouse_position.x - BOARD_OFFSET_X)/ (TILE_SIZE))
-	var pos = rows * GRID_SIZE + cols #Posição no vetor unidimensional
+	var rows: int = int((mouse_position.y - BOARD_OFFSET_Y) / (tile_size))
+	var cols: int = int((mouse_position.x - BOARD_OFFSET_X) / (tile_size))
+	var pos: int = rows * grid_size + cols #Posição no vetor unidimensional
 
-	var empty = find_empty()
-	var neighbours = empty_neighbours(empty)
+	#Se a peça clicacada for toltamente cercada é ignorada
+	if restrictions[pos] == "locked":
+		return
+		
+	var empty: int = find_empty()
+	var neighbours: Array = empty_neighbours(empty, restrictions)
 
 	#Verifica se a peça que o jogador clicou é vizinha ao espaço vazio
 	if pos in neighbours:
@@ -129,28 +210,28 @@ func handle_mouse_click(mouse_position):
 		print("Resolvido")
 		shuffle_tiles()
 
-
 func is_solved() -> bool:
-	var aux = []
-	var actual_rows = []
+	var aux: Array = []
+	var actual_rows: Array = []
 
 	#Verifica se o espaço vazio está na ultima posição(canto inferior direito) (verificação mais rapida)
-	if find_empty() != 15:
+	if find_empty() != tile_count - 1:
 		return false
 
 	#Meio repetitivo provalmente tem uma solução melhor
-	for i in range(0, 16):
+	#Transforma o vetor unidimension do estado atual do tabuleiro em um vetor MxN
+	for i in range(0, tile_count):
 		aux.append(tiles[i].name)
-		if (i + 1) % 4 == 0:
+		if (i + 1) % grid_size == 0:
 			actual_rows.append(aux)
 			aux = []
-
+	
 	"""Diferente do 15tile padrão que a ordem é numérica de 1a16, aqui precisa
 	estar apenas com cada elemento na sua linha correta sem importar a coluna"""
 	for i in range(0, actual_rows.size()):
 		actual_rows[i].sort()
 		expected_rows[i].sort()
-
+		
 		#Caso um elemento esteja fora da sua linha esperada retorna falso
 		if actual_rows[i] != expected_rows[i]:
 			return false
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,12 +2,29 @@ extends Area2D
 
 @onready var spawn_point = $Character
 @onready var username_space = $Username
+#@onready var 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	"""
+	var background = $Background as Sprite2D
+	
+	var window_size = get_window().size
+	var texture_size = background.texture.get_size()
+	
+	window_size.x = 720
+	window_size.y = 1600
+	#0.083 90
+	#print(texture_size.x * 0.2) #10x17 90x153 (1080, 1836) 
+	print(texture_size.x)
+	print(texture_size.y)
+	background.scale.x=  (texture_size.x / window_size.x) * 0.1
+	background.scale.y = (texture_size.y / window_size.y) * 0.1
+	print(background.scale.x)
+	print(background.scale.y) """
+
 	#if Global.personagem_selecionado != null:
 	#	var spawn_point = $Character
-
 	# Adiciona o personagem como filho do spawn point
 	spawn_point.add_child(Global.personagem_selecionado)
 	username_space.add_child(Global.username)
@@ -15,5 +32,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
