687bda55f2f560f82721aeade1262336484a58e7
Tiago Oliveira <tiagofelipe@alunos.utfpr.edu.br>
2025-01-24 21:38:54 -0300

    Adicionado adaptação do tabuleiro para diferentes tamanhos, geração do tabuleiro de acordo com informações dos arquivos level{i}.json

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,26 +1,104 @@
 extends Area2D
 
 #OBS: Precisa generalizar para funcionar em tabuleiros 3x3, 4x4, 5x5
-var grid_size: int = 4
-var tile_count: int = grid_size * grid_size
+
 const BOARD_OFFSET_X: int = 3 #Espaço da borda da tela
 const BOARD_OFFSET_Y: int = 42
+const BOARD_SIZE: int = 84
+const TILES_COLORS = [Color(1.0, 0.0, 0.0), Color(1.0, 0.0, 1.0), Color(1.0, 1.0, 0.0),
+ Color(0.0, 1.0, 0.0), Color(1.0, 0.67, 0.11)]
+# Carrega as frutas e os fundos
+const FRUIT_SCENE:PackedScene = preload("res://actors/fruit.tscn") 
+const EMPTY_SCENE:PackedScene = preload("res://actors/empty.tscn")
+const BACKGROUND_SCENE:PackedScene = preload("res://actors/tile_background.tscn")
 
-var scale_tile: float = 0.035
-var tile_size: float = 600 * scale_tile
+var grid_size: int
+var tile_count: int
+var scale_tile: float
+var tile_size: float
 var tiles: Array = []
-var solved: Array = []
-var expected_rows: Array = []
+var solved_rows: Array = []
 var restrictions: Array = [] 
 var fences: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var screen_size: Vector2 = get_viewport().get_visible_rect().size
-	var tile: Sprite2D = $Frutas/Tile0
-	var sprite: Sprite2D = $Background
+	adjust_background()
+	start_game()
 
-	var texture_size: Vector2 = sprite.texture.get_size()
+func start_game() -> void:
+	#Le informações do nivel do level{i}.json
+	var file:FileAccess = FileAccess.open("res://levels/level5.json", FileAccess.READ)
+	var text:String = file.get_as_text()
+	var data:Dictionary = JSON.parse_string(text)
+
+	#Define valores para a construção do tabuleiro
+	var map:Array = data["map"] #O mapa com a ordem que as peças estarão
+	grid_size = (data["size"]) #Tamanho do map
+	tile_count  = grid_size * grid_size #Quantidade de peças
+	restrictions = data["restrictions"] #Lugares onde há restrições(cercas)
+	tile_size = BOARD_SIZE / grid_size #Define o tamanho da peça em pixels
+	scale_tile = 0.14 / grid_size #Escala para transformar o sprite original no tamanho do tile_size
+	
+	# Referências para os nós pais ondes as frutas e o fundo serão instanciados
+	var fruits_parent:Node2D = get_node("Frutas")
+	var backgrounds_parent:Node2D = get_node("FundosPeca")
+
+	#Ajusta local da peças, já que a escala aumenta o tamanho da peças em todos sentidos
+	var tile_offset:float = 42 / grid_size 
+	
+	#Instacia cada fundo
+	for i in range(grid_size):
+		for j in range(grid_size):
+			var background_instance: Sprite2D
+			background_instance = BACKGROUND_SCENE.instantiate()
+			background_instance.scale = Vector2(scale_tile, scale_tile)
+			
+			#Muda a cor do fundo de acordo com  a linha atual
+			background_instance.modulate = TILES_COLORS[i] 
+			background_instance.position = Vector2((tile_size * j + tile_offset), tile_size * i + tile_offset)
+			backgrounds_parent.add_child(background_instance)
+	
+	#Instacia os sprites da fruta
+	for i in range(tile_count):
+		var fruit_instance:Sprite2D = FRUIT_SCENE.instantiate()
+		var fruit_texture:Texture
+		
+		if map[i].contains("Apple"):
+			fruit_texture = load("res://sprites/Fruits/AppleNL_HL.png") 
+		elif map[i].contains("Grape"):
+			fruit_texture = load("res://sprites/Fruits/PurplegrapeNL_HL.png") 
+		elif map[i].contains("Banana"):
+			fruit_texture = load("res://sprites/Fruits/BananaNL_HL.png")
+		elif map[i].contains("Pear"):
+			fruit_texture = load("res://sprites/Fruits/PearNL_HL.png")
+		elif map[i].contains("Orange"):
+			fruit_texture = load("res://sprites/Fruits/OrangeNL_HL.png")
+		elif map[i].contains("Empty"):
+			fruit_instance = EMPTY_SCENE.instantiate()
+			
+		fruit_instance.name = map[i] 
+		fruit_instance.texture = fruit_texture
+		
+		tiles.append(fruit_instance) #Adiciona a instacia no array do estado atual do board
+		fruits_parent.add_child(fruit_instance)
+		
+	var scale_fruit:float = 2.0 / grid_size 
+	for i in range(grid_size):
+		for j in range(grid_size):
+			tiles[grid_size * i + j].position = Vector2(tile_size * j \
+				+ tile_offset, tile_size * i + tile_offset)
+			tiles[grid_size * i + j].scale = Vector2(scale_fruit, scale_fruit)
+	
+	solved_rows = data["solved"]
+	instantiate_fences(restrictions)
+
+func adjust_background() -> void:
+	#Pega o tamanho da tela do dispositivo
+	var screen_size: Vector2 = get_viewport().get_visible_rect().size
+	var background: Sprite2D = $Background
+
+	var texture_size: Vector2 = background.texture.get_size()
 
 	# Calcular escala
 	var scale_x: float = screen_size.x / texture_size.x
@@ -29,72 +107,43 @@ func _ready() -> void:
 	var scale_factor: float = max(scale_x, scale_y)
 
 	# Ajustar o sprite com o fator de escala
-	sprite.scale = Vector2(scale_factor, scale_factor)
+	background.scale = Vector2(scale_factor, scale_factor)
 	
 	# Ajustar a posição do sprite para manter o fundo centralizado
 	var offset_x: float = (screen_size.x - texture_size.x * scale_factor) / 2
 	var offset_y: float = (screen_size.y - texture_size.y * scale_factor) / 2
-	sprite.position = Vector2(offset_x + 42, offset_y + 58)
-	
-	start_game()
-
-func start_game() -> void:
-	#Referencia para os sprites
-	tiles = [
-		$Frutas/Tile1, $Frutas/Tile2, $Frutas/Tile3, $Frutas/Tile4,
-		$Frutas/Tile5, $Frutas/Tile6, $Frutas/Tile7, $Frutas/Tile8,
-		$Frutas/Tile9, $Frutas/Tile10, $Frutas/Tile11, $Frutas/Tile12,
-		$Frutas/Tile13, $Frutas/Tile14, $Frutas/Tile15, $Frutas/Tile0
-	]
-	restrictions = [
-		"free", "free", "locked", "horizontal_locked",
-		"free", "free", "free", "free",
-		"free", "vertical_locked", "free", "free",
-		"free", "free", "free", "free",
-	]
-
-	var aux: Array = []
-	for i in range(0, tile_count):
-		aux.append(tiles[i].name)
-		if (i + 1) % grid_size == 0:
-			expected_rows.append(aux)
-			aux = []
-
-	instantiate_fences(restrictions)
-	shuffle_tiles()
+	background.position = Vector2(offset_x + 42, offset_y + 58)
 
 func instantiate_fences(restrictions: Array) -> void:
 	#Dependendo do tipo de restrição instancia a cena da cerca
 	var scene: PackedScene
 	for i in range(len(restrictions)):
-		if restrictions[i] == "free":
-			fences.append(null)
-			continue
-		elif restrictions[i] == "locked":
-			scene = load("res://actors/fullFence.tscn") 
-			
-		elif restrictions[i] == "horizontal_locked":
-			scene = load("res://actors/verticalFence.tscn")
+		match restrictions[i]:
+			"free":
+				fences.append(null)
+				continue
+			"locked":
+				scene = load("res://actors/fullFence.tscn")
+			"horizontal_locked":
+				scene = load("res://actors/verticalFence.tscn")
+			"vertical_locked":
+				scene = load("res://actors/horizontalFence.tscn")
 
-		elif restrictions[i] == "vertical_locked":
-			scene = load("res://actors/horizontalFence.tscn")
-
-		var instance: Node2D = scene.instantiate()			
+		var instance: Node2D = scene.instantiate()
 		#Calcula a posição da cerca na tela de acordo com a posição da restrição
 		instance.position.x += (i % grid_size) * tile_size
 		instance.position.y += (i / grid_size) * tile_size
+		instance.scale = Vector2(4.0 / grid_size, 4.0 / grid_size)
+		
 		add_child(instance)
 		fences.append(instance)
 
 #Procura pelo espaço vazio
 func find_empty() -> int:
-	var count: int = 0
-	for t in tiles:
-		if t == $Frutas/Tile0:
-			break
-		count += 1
-
-	return count
+	for count in range(tile_count):
+		if tiles[count] == $Frutas/Empty:
+			return count
+	return -1
 
 #Verifica quais são os vizinhos ortoganais do espaço vazinho
 func empty_neighbours(empty: int, restrictions: Array) -> Array:
@@ -112,6 +161,13 @@ func empty_neighbours(empty: int, restrictions: Array) -> Array:
 		#Ignora vizinhos que estrapolam os limites do tabuleiro
 		if neighbour < 0 or neighbour >= tile_count:
 			continue
+		
+		#Impede que a peça mova da ultima coluna para a primeira da próxima linha e vice-versa
+		#Att: é necessaria :)
+		if direction == 1 and empty % grid_size == grid_size - 1:
+			continue  
+		if direction == -1 and empty % grid_size == 0:
+			continue  
 		
 		#Caso haja uma restrição horizontal, ignora as movimentação para esquerda e direita
 		if direction in [1, -1] and restrictions[neighbour] == "horizontal_locked":
@@ -166,14 +222,7 @@ func swap_tiles(tile_src: int, tile_dst: int) -> void:
 	var temp_restriction: String = restrictions[tile_src]
 	restrictions[tile_src] = restrictions[tile_dst]
 	restrictions[tile_dst] = temp_restriction
-	
-	#print(fences)
-	#print(temp_restriction)
-	#		instance.position.x += (i % grid_size) * tile_size
-	#	instance.position.y += (i / grid_size) * tile_size
-	print(tile_src)
-	print(tile_dst)
-	print("\n")
+
 	var temp_fence: Node2D = fences[tile_src]
 	fences[tile_src] = fences[tile_dst]
 	fences[tile_dst] = temp_fence
@@ -221,19 +270,21 @@ func is_solved() -> bool:
 	#Meio repetitivo provalmente tem uma solução melhor
 	#Transforma o vetor unidimension do estado atual do tabuleiro em um vetor MxN
 	for i in range(0, tile_count):
-		aux.append(tiles[i].name)
+		aux.append(str(tiles[i].name))
 		if (i + 1) % grid_size == 0:
 			actual_rows.append(aux)
 			aux = []
 	
+	print(actual_rows)
+	#print(solved_rows)
 	"""Diferente do 15tile padrão que a ordem é numérica de 1a16, aqui precisa
 	estar apenas com cada elemento na sua linha correta sem importar a coluna"""
 	for i in range(0, actual_rows.size()):
 		actual_rows[i].sort()
-		expected_rows[i].sort()
+		solved_rows[i].sort()
 		
 		#Caso um elemento esteja fora da sua linha esperada retorna falso
-		if actual_rows[i] != expected_rows[i]:
+		if actual_rows[i] != solved_rows[i]:
 			return false
 
 	return true
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,29 +2,12 @@ extends Area2D
 
 @onready var spawn_point = $Character
 @onready var username_space = $Username
-#@onready var 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	"""
-	var background = $Background as Sprite2D
-	
-	var window_size = get_window().size
-	var texture_size = background.texture.get_size()
-	
-	window_size.x = 720
-	window_size.y = 1600
-	#0.083 90
-	#print(texture_size.x * 0.2) #10x17 90x153 (1080, 1836) 
-	print(texture_size.x)
-	print(texture_size.y)
-	background.scale.x=  (texture_size.x / window_size.x) * 0.1
-	background.scale.y = (texture_size.y / window_size.y) * 0.1
-	print(background.scale.x)
-	print(background.scale.y) """
-
 	#if Global.personagem_selecionado != null:
 	#	var spawn_point = $Character
+	
 	# Adiciona o personagem como filho do spawn point
 	spawn_point.add_child(Global.personagem_selecionado)
 	username_space.add_child(Global.username)
